cda440a193f78728d2539a047adf584ffe6d5805
Jasper Drescher <jasperdre@gmail.com>
2025-12-24 16:30:08 +0900

    Add levels and basic game mode

---
+++ Cookie/Source/Cookie/Core/CkGameMode.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CkGameMode.generated.h"
+
+UCLASS()
+class COOKIE_API ACkGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
