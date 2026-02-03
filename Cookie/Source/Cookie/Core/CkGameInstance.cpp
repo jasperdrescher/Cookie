3150ee48b99498cb802246b8dcef7a81b67fcfe3
Jasper Drescher <jasperdre@gmail.com>
2026-02-03 18:25:57 +0900

    Add rich presence test

--- Cookie/Source/Cookie/Core/CkGameInstance.cpp
+++ Cookie/Source/Cookie/Core/CkGameInstance.cpp
@@ -1,3 +1,42 @@
 // Copyright (c) 2025 Jasper Drescher. All rights reserved.
 
 #include "Core/CkGameInstance.h"
+
+#include "Cookie.h"
+#include "ThirdParty/Steamworks/Steamv161/sdk/public/steam/steam_api.h"
+
+void UCkGameInstance::SetSteamRichPresence(const FString& Key, const FString& Value)
+{
+	if (SteamAPI_Init() && SteamFriends() != nullptr)
+	{
+		UE_LOG(LogCookie, Log, TEXT("Attempting to set Steam Rich Presence for key: %s, value: %s"), *Key, *Value);
+
+		const bool bResult = SteamFriends()->SetRichPresence(TCHAR_TO_UTF8(*Key), TCHAR_TO_UTF8(*Value));
+		if (bResult)
+		{
+			UE_LOG(LogCookie, Log, TEXT("Set Steam Rich Presence"));
+		}
+		else
+		{
+			UE_LOG(LogCookie, Warning, TEXT("Failed to set Steam Rich Presence"));
+		}
+	}
+	else
+	{
+		UE_LOG(LogCookie, Warning, TEXT("Steam is not running or the SteamFriends interface is unavailable"));
+	}
+}
+
+void UCkGameInstance::ClearSteamRichPresence()
+{
+	if (SteamAPI_Init() && SteamFriends() != nullptr)
+	{
+		UE_LOG(LogCookie, Log, TEXT("Attempting to clear Steam Rich Presence"));
+
+		SteamFriends()->ClearRichPresence();
+	}
+	else
+	{
+		UE_LOG(LogCookie, Warning, TEXT("Steam is not running or the SteamFriends interface is unavailable"));
+	}
+}
--- Cookie/Source/Cookie/Core/CkGameInstance.h
+++ Cookie/Source/Cookie/Core/CkGameInstance.h
@@ -4,12 +4,19 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "AdvancedFriendsGameInstance.h"
 
 #include "CkGameInstance.generated.h"
 
 UCLASS()
-class COOKIE_API UCkGameInstance : public UGameInstance
+class COOKIE_API UCkGameInstance : public UAdvancedFriendsGameInstance
 {
 	GENERATED_BODY()
 	
+public:
+	UFUNCTION(BlueprintCallable, Category = "GameInstance")
+	void SetSteamRichPresence(const FString& Key, const FString& Value);
+
+	UFUNCTION(BlueprintCallable, Category = "GameInstance")
+	void ClearSteamRichPresence();
 };
