dc5cc40e808510c3c6ed5154327614d8f692af4b
Jasper Drescher <jasperdre@gmail.com>
2026-03-09 17:47:01 +0900

    Inherit from base game target

--- Cookie/Source/CookieGameEOS.Target.cs
+++ Cookie/Source/CookieGameEOS.Target.cs
@@ -3,16 +3,10 @@
 using UnrealBuildTool;
 using System.Collections.Generic;
 
-public class CookieGameEOSTarget : TargetRules
+public class CookieGameEOSTarget : CookieGameTarget
 {
 	public CookieGameEOSTarget(TargetInfo Target) : base(Target)
 	{
-		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
-
-		ExtraModuleNames.Add("Cookie");
-
 		CustomConfig = "EOS";
 
 		EnablePlugins.AddRange(
